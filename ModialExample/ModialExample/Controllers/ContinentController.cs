b18704faf2fcdab59588c6a533f097a5c1acb2c5
drjofu <drjofu@web.de>
2023-03-31 10:54:25 +0200

    Day 3

---
+++ ModialExample/ModialExample/Controllers/ContinentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModialExample.Models;
+using Shared;
+
+namespace ModialExample.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class ContinentController : ControllerBase
+  {
+    private readonly World world;
+
+    public ContinentController(World world)
+    {
+      this.world = world;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Continent>> Get()
+    {
+      return Ok(world.GetContinents());
+    }
+  }
+}
---
+++ ModialExample/ModialExample/Controllers/CountryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModialExample.Models;
+using Shared;
+
+namespace ModialExample.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class CountryController : ControllerBase
+  {
+    [HttpGet]
+    public ActionResult<IEnumerable<Country>>Get(string continentId, [FromServices]World world)
+    {
+      var countries = world.GetCountriesByContinentId(continentId);
+      if (countries.Any())
+        return Ok(countries);
+
+      return NotFound($"Continent with id {continentId} is not available");
+    }
+  }
+}
